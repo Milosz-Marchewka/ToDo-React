49caf0d4d3b8607e63ba809636d3e534ac69aa9f
MiloMilek <milomilekyes@gmail.com>
2025-06-20 11:31:26 +0200

    Sorting by due date, create date and status. Datetime input for due date.

--- src/TodoForm.js
+++ src/TodoForm.js
@@ -5,17 +5,29 @@ import { useState } from "react";
 function TodoForm({addTodo}){
     const [task, setTask] = useState("");
     const [description, setDescription] = useState("");
+    const [dueDate, setDueDate] = useState(toDateTimeLocalString());
     const [showEmpty, setShowEmpty] = useState(false);
+
+    function toDateTimeLocalString(date = new Date()){
+        const pad = (num)=>num.toString().padStart(2,'0');
+        const year = date.getFullYear();
+        const month = pad(date.getMonth() + 1);
+        const day = pad(date.getDate());
+        const hour = pad(date.getHours());
+        const minute = pad(date.getMinutes());
+        return `${year}-${month}-${day}T${hour}:${minute}`;
+    }
 
     const handleSubmit = (e)=>{
         e.preventDefault();
 
-        const date = new Date(Date.now()).getTime();
+        const date = new Date().getTime();
         const status = false;
         if(task.trim().length > 0){
-            addTodo({ task, description, status, date});
+            addTodo({ task, description, status, date, dueDate});
             setTask('');
             setDescription('');
+            setDueDate(toDateTimeLocalString());
         } else {
             setShowEmpty(true);
         }
@@ -27,12 +39,17 @@ function TodoForm({addTodo}){
     const handleTextarea = (e)=>{
         setDescription(e.target.value);
     }
+    const handleDueDate = (e)=>{
+        setDueDate(e.target.value);
+    }
 
     return(
         <>
             <form onSubmit={handleSubmit}>
-                <input type="text" value={task} onChange={handleTask} placeholder="Task name"/><br/>
-                <textarea value={description} onChange={handleTextarea} placeholder="Description"/><br/>
+                <input type="text" value={task} onChange={handleTask} placeholder="Task name"/>
+                <textarea value={description} onChange={handleTextarea} placeholder="Description"/>
+                <label>Due date</label>
+                <input type="datetime-local" value={dueDate} onChange={handleDueDate} />
                 <button type="submit" className="form-button">Add</button>
             </form>
             { showEmpty &&
--- src/TodoTable.js
+++ src/TodoTable.js
@@ -9,7 +9,15 @@ function TodoTable({todos, setTodos}){
     const [editIndex, setEditIndex] = useState(0);
     const [showModal, setShowModal] = useState(false);
     const [showEdit, setShowEdit] = useState(false);
-    const sorted = [...todos].sort((a,b)=>a.status-b.status);
+    const [isSortedByDueDate, setIsSortedByDueDate] = useState(false);
+    const [isSortedByCreateDate, setIsSortedByCreateDate] = useState(false);
+    const [isSortedByStatus, setIsSortedByStatus] = useState(false);
+    const sortSvgs = {
+        up: <svg width="25px" height="25px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M7 15L12 10L17 15" stroke="#000000" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+            </svg>,
+        down: <svg width="25px" height="25px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M7 10L12 15L17 10" stroke="#000000" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path> </g></svg>
+    }
 
     const deleteRow = (indexDelete)=>{
         const updatedTodos = todos.filter((_, index)=> index !== indexDelete);
@@ -32,6 +40,54 @@ function TodoTable({todos, setTodos}){
         setEditIndex(index);
         setShowEdit(true);
     }
+    const handleSortByDueDate = ()=>{
+        let updatedTodos;
+        if(!isSortedByDueDate){
+            updatedTodos = [...todos].sort((a,b)=>new Date(a.dueDate)-new Date(b.dueDate));
+            setIsSortedByDueDate(true);
+            setIsSortedByCreateDate(false);
+            setIsSortedByStatus(false);
+        } else {
+            updatedTodos = [...todos].reverse();
+            setIsSortedByDueDate(false);
+            setIsSortedByCreateDate(false);
+            setIsSortedByStatus(false);
+        }
+        setTodos(updatedTodos);
+        localStorage.setItem("todos", JSON.stringify(updatedTodos));
+    }
+    const handleSortByCreateDate = ()=>{
+        let updatedTodos;
+        if(!isSortedByCreateDate){
+            updatedTodos = [...todos].sort((a,b)=>new Date(a.date)-new Date(b.date));
+            setIsSortedByCreateDate(true);
+            setIsSortedByDueDate(false);
+            setIsSortedByStatus(false);
+        } else {
+            updatedTodos = [...todos].reverse();
+            setIsSortedByCreateDate(false);
+            setIsSortedByDueDate(false);
+            setIsSortedByStatus(false);
+        }
+        setTodos(updatedTodos);
+        localStorage.setItem("todos", JSON.stringify(updatedTodos));
+    }
+    const handleSortByStatus = ()=>{
+        let updatedTodos;
+        if(!isSortedByCreateDate){
+            updatedTodos = [...todos].sort((a,b)=>a.status-b.status);
+            setIsSortedByCreateDate(true);
+            setIsSortedByDueDate(false);
+            setIsSortedByStatus(false);
+        } else {
+            updatedTodos = [...todos].reverse();
+            setIsSortedByCreateDate(false);
+            setIsSortedByDueDate(false);
+            setIsSortedByStatus(false);
+        }
+        setTodos(updatedTodos);
+        localStorage.setItem("todos", JSON.stringify(updatedTodos));
+    }
 
     return(
         (todos.length > 0) && 
@@ -41,20 +97,35 @@ function TodoTable({todos, setTodos}){
                         <tr>
                             <th>Task</th>
                             <th>Description</th>
-                            <th>Date</th>
                             <th>
-                                Status
+                                <div className="sortTh">
+                                    <button className="sortBtn" onClick={handleSortByDueDate}>Due date</button>
+                                    {isSortedByDueDate ? sortSvgs.up : sortSvgs.down}
+                                </div>
+                            </th>
+                            <th>
+                                <div className="sortTh">
+                                    <button className="sortBtn" onClick={handleSortByCreateDate}>Date created</button>
+                                    {isSortedByCreateDate ? sortSvgs.up : sortSvgs.down}
+                                </div>
+                            </th>
+                            <th>
+                                <div className="sortTh">
+                                    <button className="sortBtn" onClick={handleSortByStatus}>Status</button>
+                                    {isSortedByStatus ? sortSvgs.up : sortSvgs.down}
+                                </div>
                             </th>
                             <th></th>
                         </tr>
                     </thead>
                     <tbody>
                         {
-                        sorted.map((todo, index)=>(
+                        todos.map((todo, index)=>(
                             <tr key={index}>
                                 <td>{todo.task}</td>
                                 <td>{todo.description}</td>
-                                <td>{new Date(todo.date).toUTCString()}</td>
+                                <td>{new Date(todo.dueDate).toLocaleString().slice(0,-3)}</td>
+                                <td>{new Date(todo.date).toLocaleString().slice(0,-3)}</td>
                                 <td>{<Status status={todo.status}/>}</td>
                                 <td className="controls">
                                     {(!todo.status) ? <button onClick={()=>setDone(index)} className = "done">Done!</button> : ''}
